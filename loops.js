137623a7bfe29d3b3f97dba0a899a726372967b5
Poonam <poonamsodani09@gmail.com>
2018-04-04 23:57:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
-  for(let i=0;i<25;i++){
-    if(i === 0)
+  for(let i=1;i<=25;i++){
+    if(i === 1)
     array[i] = "I am ${i} strange loop"
     else
     array[i] = "I am ${i} strange loops"
@@ -12,4 +12,8 @@ function whileLoop(number){
   while(number>0)
   console.log(number--)
   return 'done'
+}
+
+function doWhileLoop(array){
+  
 }
